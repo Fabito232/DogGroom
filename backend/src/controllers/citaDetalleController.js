c6239bdc051cc2761fe3a8cc0dd93ef02c0d2619
Fabito232 <fabibbsi6@gmail.com>
2024-05-20 22:26:37 -0600

    cambios de nombre

--- backend/src/controllers/citaDetalleController.js
+++ backend/src/controllers/citaDetalleController.js
@@ -1,4 +1,4 @@
-import CitaDetalle from '../models/citaDetalle.js'
+import CitaDetalle from '../models/citaDetalleModel.js'
 import sequelize from '../database/database.js';
 
 export const createCitaDetalle = async (req, res) => {
