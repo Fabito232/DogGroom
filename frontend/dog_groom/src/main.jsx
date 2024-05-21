0f9608fd29e274a86b920e855d8ec578deb9d452
Javibr26 <javibr525@gmail.com>
2024-05-21 14:42:24 -0600

    lista_productos

--- frontend/dog_groom/src/main.jsx
+++ frontend/dog_groom/src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Lista_productos from './lista_productos.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Lista_productos/>
+
   </React.StrictMode>,
 )
