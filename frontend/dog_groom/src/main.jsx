402c1a23c75eb4ffb5c9d1ee3f71f4df3f8fec21
Javibr26 <javibr525@gmail.com>
2024-05-23 23:27:42 -0600

    Agregar, Editar y Eliminar Funcionales

--- frontend/dog_groom/src/main.jsx
+++ frontend/dog_groom/src/main.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Lista_productos from './lista_productos.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Lista_productos/>
-
+    <App />
   </React.StrictMode>,
 )
+
+//<Lista_productos/>
+//<Agregar_producto/>
