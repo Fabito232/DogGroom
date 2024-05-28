e152e4a1435460cab90ae4495ac41953fbb7e9b8
Fabito232 <fabibbsi6@gmail.com>
2024-05-28 00:04:36 -0600

    Revert "Merge pull request #7 from Fabito232/javier"
    This reverts commit fae58869b9a38767ec80f20570f5a493d0935f98, reversing
    changes made to 5217731f137401da9c41d6ecbd09b92ebd91e7c8.

---
+++ frontend/dog_groom/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Lista_productos from './components/lista_productos';
+
+
+const App = () => {
+  return (
+      <Router>
+          <Routes>
+              <Route path="/" element={<Lista_productos />} />
+          </Routes>
+      </Router>
+  );
+}
+
+export default App
