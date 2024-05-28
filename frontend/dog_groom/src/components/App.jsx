d231cae13ae3e03697bf2c293e03a85facaf958d
Fabito232 <fabibbsi6@gmail.com>
2024-05-27 22:49:21 -0600

    Revert "Merge branch 'javier' of https://github.com/Fabito232/DogGroom into fabian"
    This reverts commit dbdaf3564aa97d6d383210bde59ca80ab219fd6e, reversing
    changes made to 999121e243ffa805bf1f4214855230f38d8aab56.

--- frontend/dog_groom/src/components/App.jsx
+++ frontend/dog_groom/src/components/App.jsx
@@ -6,7 +6,6 @@ import Login from './login'
 import Citas from './citas'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Lista_productos from './lista_productos';
 
 const App = () => {
   
@@ -18,10 +17,10 @@ const App = () => {
           <Route path="/" element={<Login/>}/>
           <Route path="/citas" element={<Citas />}/>
           <Route path="/agendarcita" element={<AgendarCita />}/>
-          <Route path="/productos" element={<Lista_productos />} />
         </Routes>
       </Router>
     </>
   )
 }
 export default App
+
